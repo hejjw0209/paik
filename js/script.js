4bec64e6142c02decb6572336225a0dc75d48002
hjw <finofarina@naver.com>
2025-01-10 14:54:33 +0900

    250110-메인페이지 1차 완성

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const visualSlider = new Swiper(".visual-slider", {
+    loop: true, // 반복
+    autoplay: false,
+    speed: 1000, // 기본값은 300ms
+
+    spaceBetween: 0, // 사이 간격(px)
+    centeredSlides: true, // 첫번째 슬라이드를 가운데로
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+
+        clickable: true, // 클릭가능
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".btn-next",
+        prevEl: ".btn-prev",
+    },
+});
+const menuSlider = new Swiper(".menu-slider", {
+    loop: true, // 반복
+    autoplay: false,
+    speed: 1000, // 기본값은 300ms
+
+    spaceBetween: 0, // 사이 간격(px)
+    centeredSlides: true, // 첫번째 슬라이드를 가운데로
+
+    // If we need pagination
+    pagination: {
+        el: ".menu-slider-wrap .swiper-pagination",
+
+        clickable: true, // 클릭가능
+    },
+});
+const snsSlider = new Swiper(".sns-slider", {
+    loop: true, // 반복
+    autoplay: false,
+    speed: 1000, // 기본값은 300ms
+
+    spaceBetween: 20, // 사이 간격(px)
+    centeredSlides: true, // 첫번째 슬라이드를 가운데로
+    slidesPerView: 3,
+
+    // If we need pagination
+    pagination: {
+        el: ".sns-slider-wrap .swiper-pagination",
+
+        clickable: true, // 클릭가능
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".btn-next",
+        prevEl: ".btn-prev",
+    },
+});
