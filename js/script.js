5d3a12272531de8d6ff17b654d0ad6b74fdebba8
hjw <finofarina@naver.com>
2025-01-16 17:10:45 +0900

    메인 반응형 작업중

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ const menuSlider = new Swiper(".menu-slider", {
 
     // If we need pagination
     pagination: {
-        el: ".swiper-pagination",
+        el: ".menu-slider-wrap .swiper-pagination",
 
         clickable: true, // 클릭가능
     },
@@ -41,7 +41,7 @@ const snsSlider = new Swiper(".sns-slider", {
 
     spaceBetween: 20, // 사이 간격(px)
     centeredSlides: true, // 첫번째 슬라이드를 가운데로
-    slidesPerView: 3,
+    slidesPerView: 1.5,
 
     // If we need pagination
     pagination: {
@@ -54,5 +54,13 @@ const snsSlider = new Swiper(".sns-slider", {
     navigation: {
         nextEl: ".btn-next",
         prevEl: ".btn-prev",
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
     },
 });
