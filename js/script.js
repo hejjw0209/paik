9830b4aa6d4a750af66b4cbb80d2dcfdcfffa50c
hjw <finofarina@naver.com>
2025-02-13 15:41:33 +0900

    꾸역꾸역

--- js/script.js
+++ js/script.js
@@ -23,85 +23,85 @@ $(function () {
         $menu.removeClass("on");
         $header.removeClass("active");
     });
-});
 
-const visualSlider = new Swiper(".visual-slider", {
-    loop: true, // 반복
-    autoplay: true,
-    speed: 1000, // 기본값은 300ms
+    const visualSlider = new Swiper(".visual-slider", {
+        loop: true, // 반복
+        autoplay: true,
+        speed: 1000, // 기본값은 300ms
 
-    spaceBetween: 0, // 사이 간격(px)
-    centeredSlides: true, // 첫번째 슬라이드를 가운데로
+        spaceBetween: 0, // 사이 간격(px)
+        centeredSlides: true, // 첫번째 슬라이드를 가운데로
 
-    // If we need pagination
-    pagination: {
-        el: ".swiper-pagination",
+        // If we need pagination
+        pagination: {
+            el: ".swiper-pagination",
 
-        clickable: true, // 클릭가능
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: ".btn-next",
-        prevEl: ".btn-prev",
-    },
-});
-const menuSlider = new Swiper(".menu-slider", {
-    loop: true, // 반복
-    autoplay: true,
-    speed: 1000, // 기본값은 300ms
-
-    spaceBetween: 20, // 사이 간격(px)
-    centeredSlides: true, // 첫번째 슬라이드를 가운데로
-
-    // If we need pagination
-    pagination: {
-        el: ".menu-slider-wrap .swiper-pagination",
-
-        clickable: true, // 클릭가능
-    },
-});
-const snsSlider = new Swiper(".sns-slider", {
-    loop: true, // 반복
-    autoplay: true,
-    speed: 1000, // 기본값은 300ms
-
-    spaceBetween: 20, // 사이 간격(px)
-    centeredSlides: true, // 첫번째 슬라이드를 가운데로
-    slidesPerView: 1,
-
-    // If we need pagination
-    pagination: {
-        el: ".sns-slider-wrap .swiper-pagination",
-
-        clickable: true, // 클릭가능
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: ".btn-next",
-        prevEl: ".btn-prev",
-    },
-
-    // Responsive breakpoints
-    breakpoints: {
-        500: {
-            slidesPerView: 1.5,
-            spaceBetween: 20,
+            clickable: true, // 클릭가능
         },
-    },
-    // Responsive breakpoints
-    breakpoints: {
-        700: {
-            slidesPerView: 2,
-            spaceBetween: 20,
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".btn-next",
+            prevEl: ".btn-prev",
         },
-    },
-    // Responsive breakpoints
-    breakpoints: {
-        900: {
-            slidesPerView: 3,
-            spaceBetween: 20,
+    });
+    const menuSlider = new Swiper(".menu-slider", {
+        loop: true, // 반복
+        autoplay: true,
+        speed: 1000, // 기본값은 300ms
+
+        spaceBetween: 20, // 사이 간격(px)
+        centeredSlides: true, // 첫번째 슬라이드를 가운데로
+
+        // If we need pagination
+        pagination: {
+            el: ".menu-slider-wrap .swiper-pagination",
+
+            clickable: true, // 클릭가능
         },
-    },
+    });
+    const snsSlider = new Swiper(".sns-slider", {
+        loop: true, // 반복
+        autoplay: true,
+        speed: 1000, // 기본값은 300ms
+
+        spaceBetween: 20, // 사이 간격(px)
+        centeredSlides: true, // 첫번째 슬라이드를 가운데로
+        slidesPerView: 1,
+
+        // If we need pagination
+        pagination: {
+            el: ".sns-slider-wrap .swiper-pagination",
+
+            clickable: true, // 클릭가능
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".btn-next",
+            prevEl: ".btn-prev",
+        },
+
+        // Responsive breakpoints
+        breakpoints: {
+            500: {
+                slidesPerView: 1.5,
+                spaceBetween: 20,
+            },
+        },
+        // Responsive breakpoints
+        breakpoints: {
+            700: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+        },
+        // Responsive breakpoints
+        breakpoints: {
+            900: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+        },
+    });
 });
